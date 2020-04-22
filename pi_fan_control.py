09463289b82dbf294d5c30c48fd4eb74cf00078f
leow149 <47112821+leow149@users.noreply.github.com>
2020-04-22 21:47:27 +0200

    Create pi_fan_control.py

---
+++ pi_fan_control.py
@@ -0,0 +1,35 @@
+import time
+import RPi.GPIO as GPIO
+from gpiozero import CPUTemperature
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)
+
+maxtemp = int(70)
+mintemp = int(35)
+p = GPIO.PWM(12, 25000)
+p.start(0)
+
+try:
+    while True:
+        cpu = CPUTemperature()
+        temp = int(cpu.temperature)
+        if temp >= maxtemp:
+            p.ChangeDutyCycle(100)
+            print(temp)
+            time.sleep(1)
+        elif temp <= mintemp:
+            p.ChangeDutyCycle(1)
+            print(temp)
+            time.sleep(0)
+        else:
+            speed = float(temp * 1.43)
+            gesch = round(speed)
+            p.ChangeDutyCycle(gesch)
+            print(temp)
+            time.sleep(1)
+except KeyboardInterrupt:
+    time.sleep(2)
+finally:
+    p.stop()
+    GPIO.cleanup()
