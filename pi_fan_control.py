7be28d42b33a0f7e08f5ea8b4a235665bef165e1
leow149 <47112821+leow149@users.noreply.github.com>
2020-04-23 12:08:20 +0200

    Update pi_fan_control.py

--- pi_fan_control.py
+++ pi_fan_control.py
@@ -7,31 +7,31 @@ from gpiozero import CPUTemperature
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-maxtemp = int(70)
-mintemp = int(35)
-p = GPIO.PWM(12, 25000)
-p.start(0)
+MAXTEMP = int(70)
+MINTEMP = int(35)
+P = GPIO.PWM(12, 25000)
+P.start(0)
 
 try:
     while True:
-        cpu = CPUTemperature()
-        temp = int(cpu.temperature)
-        if temp >= maxtemp:
-            p.ChangeDutyCycle(100)
-            print(temp)
+        CPU = CPUTemperature()
+        TEMP = int(CPU.temperature)
+        if TEMP >= MAXTEMP:
+            P.ChangeDutyCycle(100)
+            print(TEMP)
             time.sleep(1)
-        elif temp <= mintemp:
-            p.ChangeDutyCycle(1)
-            print(temp)
+        elif TEMP <= MINTEMP:
+            P.ChangeDutyCycle(1)
+            print(TEMP)
             time.sleep(0)
         else:
-            speed = float(temp * 1.43)
-            gesch = round(speed)
-            p.ChangeDutyCycle(gesch)
-            print(temp)
+            SPEED = float(TEMP * 1.43)
+            GESCH = round(SPEED)
+            P.ChangeDutyCycle(GESCH)
+            print(TEMP)
             time.sleep(1)
 except KeyboardInterrupt:
     time.sleep(2)
 finally:
-    p.stop()
+    P.stop()
     GPIO.cleanup()
